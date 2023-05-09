62853fa6c3343ead80b512b21568083f36638945
Jimin Baek <qorwlalsjimin@naver.com>
2023-05-09 11:07:34 +0900

    클래스 변수, ToString 메서드 오버라이딩

--- InstanceVariablePract32/Program.cs
+++ InstanceVariablePract32/Program.cs
@@ -17,10 +17,15 @@ namespace InstanceVariablePract32
             product1.name = "감자";
             product1.price = 2000;
 
+            //C# style
             Product product2 = new Product() { name = "고구마", price = 2100 };
             Product product3 = new Product() { name = "옥수수" };
             Product product4 = new Product() { price = 999999 };
             Product product5 = new Product() { price = 7777, name = "비트" };
+
+            Console.WriteLine(Product.origin);
+            Console.WriteLine(product1.name + " / " + product1.price);
+            Console.WriteLine(product1);
 
         }
     }
