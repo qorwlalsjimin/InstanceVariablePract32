1beeb34838dce023ab02323f3124560df3a5fe79
Jimin Baek <qorwlalsjimin@naver.com>
2023-05-09 10:57:27 +0900

    C# 스타일로 인스턴스 변수 초기화하기

--- InstanceVariablePract32/Program.cs
+++ InstanceVariablePract32/Program.cs
@@ -16,6 +16,12 @@ namespace InstanceVariablePract32
             product1.Equals(product1);
             product1.name = "감자";
             product1.price = 2000;
+
+            Product product2 = new Product() { name = "고구마", price = 2100 };
+            Product product3 = new Product() { name = "옥수수" };
+            Product product4 = new Product() { price = 999999 };
+            Product product5 = new Product() { price = 7777, name = "비트" };
+
         }
     }
 }
